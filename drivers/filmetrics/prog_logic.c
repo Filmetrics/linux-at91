224f93edb6d6c719252a5c681857e8b9e9ca0488
r-oberson <roberson@filmetrics.com>
2015-06-12 12:49:27 -0700

    Turns the PCK1 clock on and off
    Not fully tested yet.

--- drivers/filmetrics/prog_logic.c
+++ drivers/filmetrics/prog_logic.c
@@ -15,30 +15,51 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/clk.h>
 #include <linux/gpio.h>
 
 #define DEVICE_NAME	"prog_logic"
 
 #define PORT_OUT_SHUTDOWN  AT91_PIN_PD0
 
+static struct clk *prog_logic_clock = NULL;
+
 static int __init prog_logic_init(void)
 {
 	int err;
 
+	printk(KERN_INFO DEVICE_NAME ": init [4]\n"); ////
+
 	/* Set up Shutdown GPIO pin: "LOW" turns programmable logic chip ON */
 	err = gpio_request_one(PORT_OUT_SHUTDOWN, GPIOF_OUT_INIT_LOW,
 				"GPIO_SHUTDOWN");
+	if (err != 0) goto gpio_fail;
 
-	if (err != 0)
-		printk(KERN_WARNING DEVICE_NAME
-				": init returns error %d\n", err);
+	/* Set up programmable logic's 66 MHz clock on PCK1 */
+	prog_logic_clock = clk_get(NULL, "pck1");
+	if (prog_logic_clock == NULL) {
+		err = -1;
+		goto clk_fail;
+	}
 
-	printk(KERN_INFO DEVICE_NAME ": init [2]\n"); ////
+	err = clk_enable(prog_logic_clock);
+	if (err != 0) goto clk_fail;
+
+	return 0;  /* Init successful */
+
+clk_fail:
+	gpio_set_value(PORT_OUT_SHUTDOWN, 1);
+	gpio_direction_input(PORT_OUT_SHUTDOWN);
+	gpio_free(PORT_OUT_SHUTDOWN);
+
+gpio_fail:
+	printk(KERN_WARNING DEVICE_NAME ": init returns error %d\n", err);
 	return err;
 }
 
 static void __exit prog_logic_exit(void)
 {
+	clk_disable(prog_logic_clock);
 	gpio_set_value(PORT_OUT_SHUTDOWN, 1);
 	gpio_direction_input(PORT_OUT_SHUTDOWN);
 	gpio_free(PORT_OUT_SHUTDOWN);
