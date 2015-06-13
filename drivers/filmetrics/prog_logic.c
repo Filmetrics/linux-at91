77ad2234688f89f8eb00d7d142f78fe52a6c3715
r-oberson <roberson@filmetrics.com>
2015-06-12 18:59:30 -0700

    prog_logic driver sets PCK1 clock to 66 MHz
    - Work in progress
    - Clock is set using standard Linux clock API (linux/clk.h)
    - Uses the sama5d3_wm8904 sound driver to configure pin PD31 as PCK1 (no pinctrl
      in the prog_logic driver)
    - Has debug code that prints uC register values of PIO_PSR_D, PIO_ABCDSR1_D,
      PIO_ABCDSR2_D, and PMC_PCK1

--- drivers/filmetrics/prog_logic.c
+++ drivers/filmetrics/prog_logic.c
@@ -18,22 +18,54 @@
 #include <linux/clk.h>
 #include <linux/gpio.h>
 
+#include <linux/io.h>
+
 #define DEVICE_NAME	"prog_logic"
 
 #define PORT_OUT_SHUTDOWN  AT91_PIN_PD0
 
+#define PIO_PSR_D	0xFFFFF808
+#define PIO_ABCDSR1_D	0xFFFFF870
+#define PIO_ABCDSR2_D	0xFFFFF874
+#define PMC_PCK1	0xFFFFFC44
+
+#define PRINT_REG(reg) \
+	printk(KERN_INFO DEVICE_NAME ": " #reg " = 0x%08X\n", read_reg(reg));
+
 static struct clk *prog_logic_clock = NULL;
+
+static unsigned int read_reg(unsigned int reg_addr)
+{
+	void *reg = ioremap(reg_addr, sizeof(unsigned int));
+	return ioread32(reg);
+}
+
+static void print_regs(void)
+{
+	PRINT_REG(PIO_PSR_D);
+	PRINT_REG(PIO_ABCDSR1_D);
+	PRINT_REG(PIO_ABCDSR2_D);
+	PRINT_REG(PMC_PCK1);
+}
 
 static int __init prog_logic_init(void)
 {
 	int err;
+	struct clk *mck = NULL;
 
-	printk(KERN_INFO DEVICE_NAME ": init [4]\n"); ////
+	printk(KERN_INFO DEVICE_NAME ": init [9]\n"); ////
 
 	/* Set up Shutdown GPIO pin: "LOW" turns programmable logic chip ON */
 	err = gpio_request_one(PORT_OUT_SHUTDOWN, GPIOF_OUT_INIT_LOW,
 				"GPIO_SHUTDOWN");
 	if (err != 0) goto gpio_fail;
+
+	// Clock pin set by sama5d3_wm8904 sound driver (DTSI modified)
+
+	/// gpio_free(AT91_PIN_PD31);
+	/// at91_set_B_periph(AT91_PIN_PD31, 0);
+
+	print_regs();
 
 	/* Set up programmable logic's 66 MHz clock on PCK1 */
 	prog_logic_clock = clk_get(NULL, "pck1");
@@ -41,6 +73,17 @@ static int __init prog_logic_init(void)
 		err = -1;
 		goto clk_fail;
 	}
+
+	mck = clk_get(NULL, "mck");
+	if (mck == NULL) {
+		err = -1;
+		goto clk_fail;
+	}
+	clk_set_parent(prog_logic_clock, mck);  /// TODO - check retval...
+	clk_set_rate(prog_logic_clock, 66000000);  /// TODO - check retval...
+
+	err = clk_prepare(prog_logic_clock);
+	if (err != 0) goto clk_fail;
 
 	err = clk_enable(prog_logic_clock);
 	if (err != 0) goto clk_fail;
