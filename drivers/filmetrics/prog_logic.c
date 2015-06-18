bc43eb3afc3270ca46a8615c08e662158acb5e4d
r-oberson <roberson@filmetrics.com>
2015-06-17 17:22:51 -0700

    Adjusted programmable logic chip read timing
    - Set NRD and NCS0 pulse width to 3 clock cycles (22.7 ns) and total read cycle
      time to 5 clock cycles (37.9 ns)
    - Added comment with findings and observations
    - Turned off debugging

--- drivers/filmetrics/prog_logic.c
+++ drivers/filmetrics/prog_logic.c
@@ -17,24 +17,24 @@
 #include <linux/module.h>
 #include <linux/clk.h>
 #include <linux/gpio.h>
-
 #include <linux/io.h>
 
 #define DEVICE_NAME	"prog_logic"
 
 #define PORT_OUT_SHUTDOWN  AT91_PIN_PD0
 
-#define DEBUG		1
+#define DEBUG		0
+
+#define HSMC_PULSE0	(AT91_BASE_SYS + 0x604)
+#define HSMC_CYCLE0	(AT91_BASE_SYS + 0x608)
 
 #if DEBUG
-#define HSMC_SETUP0	0xFFFFC600
-#define HSMC_PULSE0	0xFFFFC604
-#define HSMC_CYCLE0	0xFFFFC608
-#define HSMC_MODE0	0xFFFFC610
-#define PIO_PSR_D	0xFFFFF808
-#define PIO_ABCDSR1_D	0xFFFFF870
-#define PIO_ABCDSR2_D	0xFFFFF874
-#define PMC_PCK1	0xFFFFFC44
+#define HSMC_SETUP0	(AT91_BASE_SYS + 0x600)
+#define HSMC_MODE0	(AT91_BASE_SYS + 0x610)
+#define PIO_PSR_D	(AT91_BASE_SYS + 0x3808)
+#define PIO_ABCDSR1_D	(AT91_BASE_SYS + 0x3870)
+#define PIO_ABCDSR2_D	(AT91_BASE_SYS + 0x3874)
+#define PMC_PCK1	(AT91_BASE_SYS + 0x3C44)
 
 #define PRINT_REG(reg) \
 	printk(KERN_INFO DEVICE_NAME ": " #reg " = 0x%08X\n", read_reg(reg));
@@ -47,12 +47,6 @@ static unsigned int read_reg(unsigned int reg_addr)
 {
 	void __iomem *reg = ioremap(reg_addr, sizeof(unsigned int));
 	return ioread32(reg);
-}
-
-static void write_reg(unsigned int reg_addr, unsigned int value)
-{
-	void __iomem *reg = ioremap(reg_addr, sizeof(unsigned int));
-	iowrite32(value, reg);
 }
 
 static void print_regs(void)
@@ -68,17 +62,35 @@ static void print_regs(void)
 }
 #endif
 
+static void write_reg(unsigned int reg_addr, unsigned int value)
+{
+	void __iomem *reg = ioremap(reg_addr, sizeof(unsigned int));
+	iowrite32(value, reg);
+}
+
 static int __init prog_logic_init(void)
 {
 	int err;
 	struct clk *mck = NULL;
 
 #if DEBUG
-	printk(KERN_INFO DEVICE_NAME ": init [10]\n"); ////
-
-        write_reg(HSMC_PULSE0, 0x0A0A0101);
-        write_reg(HSMC_CYCLE0, 0x000E0003);
+	printk(KERN_INFO DEVICE_NAME ": init [12]\n"); ////
 #endif
+
+	/* Set NRD and NCS0 pulse width to 3 clock cycles (22.7 ns) and total
+	 * read cycle time to 5 clock cycles (37.9 ns), which leaves 1 clock
+	 * cycle setup time and 1 clock cycle hold time.
+	 * - Setting pulse width to 1 clock cycle and read cycle time to 3 clock
+	 *   cycles (at 132 MHz MCK) prevents the read cycle from completing
+	 *   properly and 0xFFFF is read out of the programmable logic chip.
+	 * - Setting pulse width to 2 clock cycles and read cycle time to 4
+	 *   clock cycles makes the read cycle work properly (0x22 read from
+	 *   Register 2), but the timing might still be marginal for the
+	 *   programmable logic chip, so we increase the read timing to 3/5 to
+	 *   have some margin.
+	 */
+	write_reg(HSMC_PULSE0, 0x03030101);
+	write_reg(HSMC_CYCLE0, 0x00050003);
 
 	/* Set up Shutdown GPIO pin: "LOW" turns programmable logic chip ON */
 	err = gpio_request_one(PORT_OUT_SHUTDOWN, GPIOF_OUT_INIT_LOW,
